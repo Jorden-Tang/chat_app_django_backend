a4a411c30254569d1e6ade945877dc92bbb8cfe7
JordenTang <Jordentangwork@gmail.com>
2020-09-29 19:28:52 -0700

    added model into admin

--- chat/models.py
+++ chat/models.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class chatRoom (models.Model){
-    room_name = models.CharField(max_length=100)
-    room_host = models.OneToOneField(User, verbose_name=_(""), on_delete=models.CASCADE)
-    room_members = models.ManyToManyField(User, verbose_name=_(""), on_delete=models.CASCADE)
-    created_at = models.DateTimeField(_(""), auto_now=False, auto_now_add=True)
-    updated_at = models.DateTimeField(_(""), auto_now=True, auto_now_add=False)
-}
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=100)
+    # room_host = models.OneToOneField(User, on_delete=models.CASCADE)
+    # room_members = models.ManyToManyField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self): # __str__ for Python 3, __unicode__ for Python 2
+        return self.name
 
 
